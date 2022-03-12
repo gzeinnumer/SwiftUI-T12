341f4069d9b9c814530c1a290fef6133688c8cd3
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-03-13 02:18:16 +0700

    Initial commit

---
+++ SwiftUI T12/SwiftUI_T12App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_T12App.swift
+//  SwiftUI T12
+//
+//  Created by M Fadli Zein on 13/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_T12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
