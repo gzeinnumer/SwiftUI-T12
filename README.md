4d201e66afcb18170f6f19c548094a0b38db3745
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-03-13 02:32:52 +0700

    commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # SwiftUI-T12
- 
+
+[Source](https://designcode.io/swiftui-handbook-max-width-and-frame-alignment)
+
+<pre>
+<img src="preview/example1.png">
+<img src="preview/example2.png">
+<img src="preview/example2.png">
+</pre>
+
+---
+
+```
+Copyright 2022 M. Fadli Zein
+```
+
--- SwiftUI T12/ContentView.swift
+++ SwiftUI T12/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Rectangle()
+                .fill(Color.blue)
+        }.overlay(
+            Image(systemName: "xmark")
+                .frame(width: 32, height: 32)
+                .background(Circle().stroke())
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+                .padding()
+        )
     }
 }
 
